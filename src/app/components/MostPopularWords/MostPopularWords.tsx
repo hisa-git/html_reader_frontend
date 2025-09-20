d8bc3fa5e47f5b3330a7538a2c4d6b95efec2b96
Kirill <kirillhisa.work@gmail.com>
2025-09-20 22:06:09 +0300

    12 attempt to deploy

--- src/app/components/MostPopularWords/MostPopularWords.tsx
+++ src/app/components/MostPopularWords/MostPopularWords.tsx
@@ -10,7 +10,7 @@ import {
 } from "@mantine/core";
 import { useState } from "react";
 
-export default function MostPopularWords(props) {
+export default function MostPopularWords(props: unknown) {
   const [showTop, setShowTop] = useState(10);
   const [showDensity, setShowDensity] = useState(10);
 
