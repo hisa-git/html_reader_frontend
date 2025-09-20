bc07770d9a16bb697cb283748d070e94d014e894
Kirill <kirillhisa.work@gmail.com>
2025-09-20 21:16:32 +0300

    2 attempt to remove a checking in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  // Дополнительная оптимизация
-  webpack: (config: any) => {
-    config.optimization.sideEffects = false;
-    return config;
-  },
 };
 
 module.exports = nextConfig;
